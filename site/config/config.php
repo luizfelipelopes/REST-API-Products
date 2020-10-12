4e2167b944addc707aff430b58121e32df05e563
Luiz Felipe <lfelipelopesti@gmail.com>
2020-10-11 21:17:34 -0300

    edited configurations app

--- site/config/config.php
+++ site/config/config.php
@@ -1,10 +1,10 @@
 <?php
 /** Configuration File */
 
-define('HOST', 'localhost');
+define('HOST', '172.17.0.1');
 define('DB', 'api_db');
 define('USER', 'root');
-define('PASS', '');
+define('PASS', '12345');
 define('DRIVE', 'mysql');
 
 $getUrl = explode('/', $_GET['url']);
