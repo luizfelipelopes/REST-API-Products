ffb4080ae5cf145cb14e31c5d943a0b12a5e5130
Luiz Felipe <lfelipelopesti@gmail.com>
2020-10-12 13:54:27 -0300

    update settings host

--- site/run-apache2.sh
+++ site/run-apache2.sh
@@ -1,3 +1,2 @@
 sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
-apache2-foreground "$@"\
-&& --env DOCKER_HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
+apache2-foreground "$@"
