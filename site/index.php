0e3108d312471b51f9d3c2a7d0393b2e9ec86740
Luiz Felipe <lfelipelopesti@gmail.com>
2020-10-12 15:27:57 -0300

    update settings host

--- site/index.php
+++ site/index.php
@@ -1,11 +1,11 @@
 <?php
-var_dump($_SERVER['REMOTE_ADDR']);
+// var_dump($_SERVER['REMOTE_ADDR']);
 
 // Get the requires necessaries
 require 'config/config.php';
 require 'model/Database.php';
 
-die;
+// die;
 
 if(isset($_GET)):
     require 'controller'. DIRECTORY_SEPARATOR . ucfirst($getUrl[0]) . '.php';
