50198be283d41ba5e1b9652398e891767ae64b82
Luiz Felipe <lfelipelopesti@gmail.com>
2020-10-17 17:15:06 -0300

    test deploy cloud sql proxy

--- site/config/config.php
+++ site/config/config.php
@@ -1,20 +1,20 @@
 <?php
 /** Configuration File */
 
-$urlDb = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $urlDb = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-define('HOST', $urlDb['host']);
-define('DB', substr($urlDb['path'], 1));
-define('USER', $urlDb['user']);
-define('PASS', $urlDb['pass']);
-define('DRIVE', 'mysql');
-
-
-
-// define('HOST', '216.239.36.53');
-// define('DB', 'api_db');
-// define('USER', 'root');
-// define('PASS', '12345');
+// define('HOST', $urlDb['host']);
+// define('DB', substr($urlDb['path'], 1));
+// define('USER', $urlDb['user']);
+// define('PASS', $urlDb['pass']);
 // define('DRIVE', 'mysql');
+
+
+
+define('HOST', 'localhost');
+define('DB', 'api_db');
+define('USER', 'apidb');
+define('PASS', '12345');
+define('DRIVE', 'mysql');
 
 $getUrl = explode('/', $_GET['url']);
