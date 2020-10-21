d77b8f1bb8d7ca9701d3df1b1d6c313cc69c4c7c
Luiz Felipe <lfelipelopesti@gmail.com>
2020-10-20 23:37:17 -0300

    update deploy just to heroku

--- site/config/config.php
+++ site/config/config.php
@@ -1,20 +1,18 @@
 <?php
 /** Configuration File */
 
-// $urlDb = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$urlDb = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-// define('HOST', $urlDb['host']);
-// define('DB', substr($urlDb['path'], 1));
-// define('USER', $urlDb['user']);
-// define('PASS', $urlDb['pass']);
-// define('DRIVE', 'mysql');
-
-
-
-define('HOST', '127.0.0.1');
-define('DB', 'api_db');
-define('USER', 'apidb');
-define('PASS', '12345');
+define('HOST', $urlDb['host']);
+define('DB', substr($urlDb['path'], 1));
+define('USER', $urlDb['user']);
+define('PASS', $urlDb['pass']);
 define('DRIVE', 'mysql');
+
+// define('HOST', '127.0.0.1');
+// define('DB', 'api_db');
+// define('USER', 'apidb');
+// define('PASS', '12345');
+// define('DRIVE', 'mysql');
 
 $getUrl = explode('/', $_GET['url']);
